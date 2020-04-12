7bdf35aba5ccbfcd1b417ec148269819ac568691
Luis Cabral <58452540+oluiscabral@users.noreply.github.com>
2020-04-12 02:23:22 -0300

    Update 1021-Notas-e-Moedas.py

--- Estruturas-de-Repeticao/1021-Notas-e-Moedas.py
+++ Estruturas-de-Repeticao/1021-Notas-e-Moedas.py
@@ -1,1 +1,19 @@
 N = float(input())
+
+nota_100 = N // 100
+
+
+print("NOTAS:")
+print(str(nota_100) + " nota(s) de R$ 100.00")
+print(str(nota_50) + " nota(s) de R$ 50.00")
+print(str(nota_20) + " nota(s) de R$ 20.00")
+print(str(nota_10) + " nota(s) de R$ 10.00")
+print(str(nota_5) + " nota(s) de R$ 5.00")
+print(str(nota_2) + " nota(s) de R$ 2.00")
+print("MOEDAS:")
+print(str(moeda_1) + " moeda(s) de R$ 1.00")
+print(str(moeda_05) + " moeda(s) de R$ 0.50")
+print(str(moeda_025) + " moeda(s) de R$ 0.25")
+print(str(moeda_010) + " moeda(s) de R$ 0.10")
+print(str(moeda_005) + " moeda(s) de R$ 0.05")
+print(str(moeda_001) + " moeda(s) de R$ 0.01")
